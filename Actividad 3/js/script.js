15a3e0e54a04cf4d868750b4def704a7133896ea
Simon Sabando Vera <151147020+ssabver156@users.noreply.github.com>
2025-01-24 11:37:33 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,8 @@
+        function pedirdatos() {
+        nombre=prompt("¿Cómo te llamas?")
+        edad=prompt("¿Qué edad tienes?")
+        alert (" Hola soy " + nombre + " y tengo "+ edad + " años ")
+        let=nombre
+        let=edad
+        }
+
