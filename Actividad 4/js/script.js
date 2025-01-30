3ecc6d03bc57e43fe62a03ff5afde746cfcb8179
Simon Sabando Vera <151147020+ssabver156@users.noreply.github.com>
2025-01-30 14:08:35 +0100

    Add files via upload

---
+++ Actividad 4/js/script.js
@@ -0,0 +1,6 @@
+         function calcular() {
+            let a = parseInt(prompt("dime el primer número"),10);
+            let b = parseInt(prompt("dime el segundo número"),10);
+            alert ("El primer número es:"+ a + "\nEl segundo número es: "  + b + "\nLa suma es: "  + (a+b) + "\nLa resta es:"  + (a-b) + "\nLa multiplicacion es:"  + (a*b) + "\nLa division es:" +  (a/b) + "\nEl resto es:"  + (a%b) +"") 
+        }
+
